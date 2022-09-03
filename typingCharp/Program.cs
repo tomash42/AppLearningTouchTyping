83ed3ff46c7c9520548b5f87644342dd8820d14f
tomek <tomash40@yandex.com>
2022-09-03 21:07:14 +0200

    update 1

--- typingCharp/Program.cs
+++ typingCharp/Program.cs
@@ -14,7 +14,8 @@ namespace typingCharp
             ConsoleKeyInfo cki;
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("LVL 1 - Learn A S D F");
+            Console.WriteLine("LVL 1 - Left Hand A S D F");
+            Console.WriteLine("LVL 2 - Right Hand J K L ;");
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Your Choice is : ");
@@ -26,7 +27,10 @@ namespace typingCharp
                 switch (menuNr)
                 {
                     case 1:
-                        lvl1();
+                        lelftHand();
+                        break;
+                    case 2:
+                        rightHand();
                         break;
                     default:
                         Console.WriteLine("App is under construction");
@@ -42,7 +46,7 @@ namespace typingCharp
 
         }
 
-        private static void lvl1()
+        private static void lelftHand()
         {
 
             //variables
@@ -98,6 +102,62 @@ namespace typingCharp
             Console.WriteLine("Good : {0} Bad :  {1}", good, bad);
             Console.ResetColor();
               }
+        private static void rightHand()
+        {
+
+            //variables
+            int sum = 0, good = 0, bad = 0, totalChar, numberSentence;
+            string[] sentence = new string[3];
+            //Timer
+            Stopwatch timeApp = new Stopwatch();
+            //array with sentence and RANDOM
+            sentence[0] = "jjjj kkkk llll ;;;; kjkj klkl l;l; ;l;l";
+            sentence[1] = "jlk; ;klj lklk ;j;j l;jk klj; ljlj ;;jj";
+            sentence[2] = "jkl; llj; lkk; l;l; l;;l ;;jj lj;; ;jjl";
+            Random rd = new Random();
+            numberSentence = rd.Next(0, sentence.Length);
+            //sb 
+            StringBuilder sb = new StringBuilder();
+            sb.Append(sentence[numberSentence]);
+            Console.WriteLine(sb);
+            //Console 
+            ConsoleKeyInfo cki;
+            //mian loop 
+
+            do
+            {
+                timeApp.Start();
+                cki = Console.ReadKey(true);
+                if (cki.KeyChar == sb[sum])
+                {
+                    Console.Beep();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(cki.KeyChar);
+                    Console.ResetColor();
+                    good++;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(cki.KeyChar);
+                    Console.ResetColor();
+                    bad++;
+                }
+                timeApp.Stop();
+                sum++;
+
+            } while (sum != sb.Length);
+
+            totalChar = sb.Length;
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("\n\n\nYour time is : {0}", timeApp.Elapsed);
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Total char is : {0}", totalChar);
+            Console.WriteLine("Good : {0} Bad :  {1}", good, bad);
+            Console.ResetColor();
+        }
 
     }
 }
